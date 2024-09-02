6adefa61868a788bec8f1b9e3c036c14ada270f9
pman <pman-$-432894328923@gmail.com>
2024-09-02 22:32:00 +0200

    chore: update file(s): test_math.py

--- tests/test_math.py
+++ tests/test_math.py
@@ -1,34 +1,37 @@
 from pyskel.math.sum import add
 
+
 def test_add_positive_numbers():
     # Given
     a = 2
     b = 3
-    
+
     # When
     result = add(a, b)
-    
+
     # Then
     assert result == 5
+
 
 def test_add_negative_numbers():
     # Given
     a = -2
     b = -3
-    
+
     # When
     result = add(a, b)
-    
+
     # Then
     assert result == -5
+
 
 def test_add_mixed_numbers():
     # Given
     a = -2
     b = 3
-    
+
     # When
     result = add(a, b)
-    
+
     # Then
     assert result == 1
