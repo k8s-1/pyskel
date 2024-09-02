9ac6dfc69ceb8a8552052ce66217c1fcb65a6d6f
pman <pman-$-432894328923@gmail.com>
2024-09-02 22:31:26 +0200

    chore: update file(s): test_api.py

--- tests/test_api.py
+++ tests/test_api.py
@@ -1,5 +1,6 @@
 from pyskel.api.request import simple_get
 
+
 def test_simple_get():
     # Given
     url = 'https://jsonplaceholder.typicode.com/todos/3'
@@ -8,4 +9,5 @@ def test_simple_get():
     response = simple_get(url)
 
     # Then
-    assert response.status_code == 200, f"Failed with response: {response.text}"
+    assert response.status_code == 200, \
+        f"Failed with response: {response.text}"
