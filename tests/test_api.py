7f5de29bdfeb820502f23a4914e90eaefda92800
pman <pman-$-432894328923@gmail.com>
2024-09-02 17:30:18 +0200

    chore: update file(s): test_api.py

--- tests/test_api.py
+++ tests/test_api.py
@@ -2,9 +2,10 @@ from python_skeleton.api.request import simple_get
 
 def test_simple_get():
     # Given
+    url = 'https://jsonplaceholder.typicode.com/todos/3'
 
     # When
-    response = simple_get()
+    response = simple_get(url)
 
     # Then
     assert response.status_code == 200, f"Failed with response: {response.text}"
