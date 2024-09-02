507aab06c98897c22bc7429899adac419eb476ec
pman <pman-$-432894328923@gmail.com>
2024-09-02 17:29:40 +0200

    chore: update file(s): request.py

--- python_skeleton/api/request.py
+++ python_skeleton/api/request.py
@@ -1,6 +1,5 @@
 import requests
 
-def simple_get():
-    url = 'https://jsonplaceholder.typicode.com/todos/1'
+def simple_get(url='https://jsonplaceholder.typicode.com/todos/1'):
     response = requests.get(url, timeout=5)
     return response
