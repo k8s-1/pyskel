2b456dba53ad344ff971f0be1c8ad580fb327f65
pman <pman-$-432894328923@gmail.com>
2024-09-02 22:29:04 +0200

    chore: update file(s): __main__.py

--- pyskel/__main__.py
+++ pyskel/__main__.py
@@ -1,12 +1,13 @@
-print("Running main.py...")
-
 from pyskel.foo import foo
 from pyskel.bar import bar
+
+print("Running main.py...")
 
 
 def main():
     print("Hello from main")
     # bar()
+
 
 if __name__ == '__main__':
     main()
