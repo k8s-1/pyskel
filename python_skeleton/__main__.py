c8af6fa0e04526ff8ff754fc8689943e2eb70523
pman <pman-$-432894328923@gmail.com>
2024-09-02 16:03:59 +0200

    chore: update file(s): pyproject.toml main.py

---
+++ python_skeleton/__main__.py
@@ -0,0 +1,11 @@
+print("running main.py...")
+
+from .foo import foo
+from .bar import bar
+
+def main():
+    print("Hello from main")
+    # bar()
+
+if __name__ == '__main__':
+    main()
--- python_skeleton/main.py
+++
@@ -1,9 +0,0 @@
-from foo import foo
-from bar import bar
-
-def main():
-    print("Hello from main")
-    bar.bar()
-
-if __name__ == '__main__':
-    main()
